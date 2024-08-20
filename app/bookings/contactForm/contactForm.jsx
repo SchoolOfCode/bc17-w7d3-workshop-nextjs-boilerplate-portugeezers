fe3f0ffa0f8893110546d86ef084fa3e3c7044be
Carmen <aisofiacatarino@gmail.com>
2024-08-20 13:55:30 +0100

    finished components controlling

--- app/bookings/contactForm/contactForm.jsx
+++ app/bookings/contactForm/contactForm.jsx
@@ -4,13 +4,33 @@ import { useState } from 'react'
 
 export default function ContactForm() {
 
-    const [ fullName, setFullName ] = useState('')
+    const [ fullName, setFullName ] = useState('');
+    const [ postcode, setPostcode] = useState("");
+    const [ address, setAddress] = useState("");
+    const [ city, setCity] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [ email, setEmail] = useState("");
 
     function handleChange(e) {
+        console.log(e.target.value);
         switch (e.target.name) {
             case "fullName":
                 setFullName(e.target.value)
                 break
+                case "postcode":
+                    setPostcode(e.target.value)
+                break
+                case "address":
+                setAddress(e.target.value)
+                break
+                case "city":
+                    setCity(e.target.value)
+                break
+                case "phoneNumber":
+                setPhoneNumber(e.target.value)
+                break
+                case "email":
+                    setEmail(e.target.value)
             default:
                 break
         }
@@ -33,17 +53,17 @@ export default function ContactForm() {
 
                     <li>
                     <label htmlFor="postcode">Postcode</label>
-                    <input type="text" name="postcode" ></input>
+                    <input type="text" name="postcode" value={postcode} onChange={handleChange}></input>
                     </li>
 
                     <li>
                     <label htmlFor="address">House/Flat Number and Street Name</label>
-                    <input type="text" name="address"></input>
+                    <input type="text" name="address" value={address} onChange={handleChange}></input>
                     </li>
 
                     <li>
                     <label htmlFor="city">City</label>
-                    <input type="text" name="city"></input>
+                    <input type="text" name="city" value={city} onChange={handleChange}></input>
                     </li>
 
                 </ul>
@@ -58,13 +78,13 @@ export default function ContactForm() {
                 <ul>
 
                     <li>
-                    <label>Phone number</label>
-                    <input></input>
+                    <label htmlFor="phoneNumber">Phone number</label>
+                    <input type="number" name="phoneNumber" value={phoneNumber} onChange={handleChange}></input>
                     </li>
 
                     <li>
-                    <label>Email address</label>
-                    <input></input>
+                    <label htmlFor="email">Email address</label>
+                    <input type="email" name="email" value={email} onChange={handleChange}></input>
                     </li>
                 </ul>
 
--- plan.md
+++ plan.md
@@ -79,8 +79,8 @@ Now all you need to do is use that bit of state ("menuToggle" maybe) to show/hid
 ### Objectives:
 
 Users should see a link on the home page saying "Book Consultation", they should not see the phone number anymore.✅
-Users should be able to click the link and go to a "Design Booking" page ("/booking")
-When a user reaches the "Design Booking" page they should see a simple form that matches the designs you've been given.
+Users should be able to click the link and go to a "Design Booking" page ("/booking")✅
+When a user reaches the "Design Booking" page they should see a simple form that matches the designs you've been given.✅
 When a user submits the form it should validate that no fields are empty.
 If there is an empty form field show an error.
 If there is no error then console log the form data 🙂 (don't worry about posting the data anywhere).
